8346b634e5d304d997d7b8a07e1c56f8af0972ed
Daniil <Daniilsemen@gmail.com>
2022-06-24 15:21:52 +0300

    e02: Setting up OpenGL and Creating a Window in C++

---
+++ src/Application.cpp
@@ -0,0 +1,31 @@
+#include "GLFW/glfw3.h"
+
+int main(void)
+{
+	// Init/Setup GLFW (window, contexts, OS messages processing)
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{ glfwTerminate(); return -1; }
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // while (!glfwWindowShouldClose(window))
+
+	glfwTerminate();
+}
