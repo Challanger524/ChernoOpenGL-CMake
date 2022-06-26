a8657fb005cc089999179e049f4b4f7b1c321cb1
Daniil <Daniilsemen@gmail.com>
2022-06-26 12:29:16 +0300

    e03: Using Modern OpenGL in C++

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,16 +1,32 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main()
 {
 	// Init/Setup GLFW (window, contexts, OS messages processing)
-	if (!glfwInit())
-		return -1;
+	glfwSetErrorCallback([](int, const char *description) noexcept { std::cerr << "Error: " << description << std::endl; });
 
-	GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+	if (!glfwInit())
+	{ std::cerr << "Error: glfwInit() fail\n"; return -1; }
+
+	GLFWwindow *window = glfwCreateWindow(640, 480, "ChernoOpenGL", nullptr, nullptr);
 	if (!window)
 	{ glfwTerminate(); return -1; }
 
 	glfwMakeContextCurrent(window);
+
+	// Init GLEW (run-time OpenGL extensions loader)
+	if (GLenum err = glewInit(); err != GLEW_OK)
+	{ std::cerr << "Error: glewInit(): " << glewGetErrorString(err) << std::endl; glfwTerminate(); return -1; }
+
+	// Print tools versions
+	std::cout << "Info: GLEW version: " << glewGetString(GLEW_VERSION) << std::endl;
+	std::cout << "Info: GL   version: " << glGetString(GL_VERSION) << std::endl;
+	std::cout << "Info: GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+	std::cout << "Info: GPU  vendor : " << glGetString(GL_VENDOR) << std::endl;
+	std::cout << "Info: Renderer    : " << glGetString(GL_RENDERER) << std::endl;
 
 	while (!glfwWindowShouldClose(window))
 	{
