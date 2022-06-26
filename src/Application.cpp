91e4e262c1c4f17028585e44c25e9eebb5ab42ed
Daniil <Daniilsemen@gmail.com>
2022-06-26 12:29:16 +0300

    e03: Using Modern OpenGL in C++

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
 int main(void)
@@ -6,6 +8,8 @@ int main(void)
 
 	/* Initialize the library */
 	if (!glfwInit()) return -1;
+
+	glfwSetErrorCallback([](int error, const char *description) { std::cerr << "Error: " << description << std::endl; });
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -17,6 +21,14 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	if (GLenum err = glewInit(); err != GLEW_OK)
+		std::cerr << "Error: glewInit(): " << glewGetErrorString(err) << std::endl;
+
+	std::cout << "Info: Vendor      : " << glGetString(GL_VENDOR) << std::endl;
+	std::cout << "Info: Renderer    : " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "Info: GLEW version: " << glewGetString(GLEW_VERSION) << std::endl;
+	std::cout << "Info: GL   version: " << glGetString(GL_VERSION) << std::endl;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
