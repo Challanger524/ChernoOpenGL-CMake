94f752b26d6d4135c5ab2e25576a3af4592a93ef
Daniil <Daniilsemen@gmail.com>
2022-06-28 20:37:43 +0300

    e11: Uniforms in OpenGL

--- src/Application.cpp
+++ src/Application.cpp
@@ -170,6 +170,7 @@ int main(void)
 	{ glfwTerminate(); return -1; }
 
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 
 	// Init GLEW (run-time OpenGL extensions loader)
@@ -215,11 +216,26 @@ int main(void)
 	unsigned int shader = CreateShader(source.vertexSource, source.fragmentSource);
 	GLCall(glUseProgram(shader));
 
+	int location;
+	GLCall(location = glGetUniformLocation(shader, "u_Color"));
+	GLASSERT(location); //ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+	float r = 0.0f;
+	float increment = 0.05f;
 	while (!glfwWindowShouldClose(window))
 	{
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r < 0.0f)
+			increment = +0.05f;
+		r += increment;
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
