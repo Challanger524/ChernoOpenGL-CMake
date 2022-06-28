9404540fbee962b0045773087e4c2cbdab928112
Daniil <Daniilsemen@gmail.com>
2022-06-28 19:45:23 +0300

    e10: Dealing with Errors in OpenGL (`glDebugMessageCallback`)
     - Attemt to check weather a debug feature is enabled with `glIsEnabled(GL_DEBUG_OUTPUT)`
     - Improved (cross-platform) debug breakpoints with `#define BREAKPOINT()` macro
     - Better `#define GLASSERT(glretval)` for return validation due to `-1`/`+0` compile-time check-aid
     - `GlDebugMessage_cb` realisation mainly inspired from [liam-middlebrook/GLDebugMessageCallback.c](https://gist.github.com/liam-middlebrook/c52b069e4be2d87a6d2f)

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,6 +6,69 @@
 #include <sstream>
 #include <string>
 #include <filesystem>
+#include <type_traits>
+
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+#include <signal.h>
+#endif
+
+#ifdef WIN32 // debug-breakpoint/platform-specific
+#define BREAKPOINT() __debugbreak()
+#elif defined (__unix__)  || (defined (__APPLE__) && defined (__MACH__))
+#define BREAKPOINT() raise(SIGTRAP)
+#else // see other methods: https://stackoverflow.com/questions/173618/is-there-a-portable-equivalent-to-debugbreak-debugbreak/49079078#49079078
+#define BREAKPOINT() // uncomment if no solutions found
+#endif // debug-breakpoint/platform-specific
+
+// Trigger debug breakpoint on `condition` fail
+#define ASSERT(condition) if (!(condition)) BREAKPOINT()
+
+// Assert GL function return type value (signed != -1 && unsigned != 0u) // So genious -_- to not use unsigned everywhere // Metaprogramming == my love :)
+#define GLASSERT(glretval) \
+	static_assert(std::is_arithmetic_v<decltype(glretval)>); \
+	if constexpr (std::is_signed_v    <decltype(glretval)>) { ASSERT(glretval != -1); } \
+	else /* if (unsigned arithmetic type)                */ { ASSERT(glretval != 0u); }
+
+static void GLAPIENTRY GlDebugMessage_cb(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *param)
+{
+	char *source_, *type_, *severity_;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             source_ = "API";             break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   source_ = "WINDOW_SYSTEM";   break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: source_ = "SHADER_COMPILER"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     source_ = "THIRD_PARTY";     break;
+	case GL_DEBUG_SOURCE_APPLICATION:     source_ = "APPLICATION";     break;
+	case GL_DEBUG_SOURCE_OTHER:           source_ = "OTHER";           break;
+	default:                              source_ = "<SOURCE>";        break;
+	}
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               type_ = "ERROR";               break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: type_ = "DEPRECATED_BEHAVIOR"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  type_ = "UDEFINED_BEHAVIOR";   break;
+	case GL_DEBUG_TYPE_PORTABILITY:         type_ = "PORTABILITY";         break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         type_ = "PERFORMANCE";         break;
+	case GL_DEBUG_TYPE_OTHER:               type_ = "OTHER";               break;
+	case GL_DEBUG_TYPE_MARKER:              type_ = "MARKER";              break;
+	default:                                type_ = "<TYPE>";              break;
+	}
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         severity_ = "HIGH";         break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       severity_ = "MEDIUM";       break;
+	case GL_DEBUG_SEVERITY_LOW:          severity_ = "LOW";          break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: severity_ = "NOTIFICATION"; break;
+	default:                             severity_ = "<SEVERITY>";   break;
+	}
+
+	std::cerr << std::setw(4) << id << ": GL " << severity_ << ' ' << type_ << " (" << source_ << "): " << message << std::endl;
+
+	BREAKPOINT();
+}
 
 struct ShaderProgramSource
 {
@@ -105,6 +168,11 @@ int main(void)
 
 	if (GLenum err = glewInit(); err != GLEW_OK)
 		std::cerr << "Error: glewInit(): " << glewGetErrorString(err) << std::endl;
+
+	// Enable OpenGL features
+	glEnable(GL_DEBUG_OUTPUT);
+	if (glIsEnabled(GL_DEBUG_OUTPUT) == GL_TRUE) // attemt to validate if gl-feature works
+		glDebugMessageCallback(GlDebugMessage_cb, nullptr);
 
 	std::cout << "Info: Vendor      : " << glGetString(GL_VENDOR) << std::endl;
 	std::cout << "Info: Renderer    : " << glGetString(GL_RENDERER) << std::endl;
