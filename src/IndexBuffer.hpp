2cb7bb68862456182db35b4bc38c9f6d908c05b1
Daniil <Daniilsemen@gmail.com>
2022-07-04 09:28:33 +0300

    e13: Abstracting OpenGL into Classes
    Note: Rename `Renderer.hpp`->`Utility.hpp` in the sake of common sense
    
    Advice:
     - use: `git --ignore-all-space <commit>` (or `-w`) to hide changes caused by an additional "{scope}"
     - in case of GitHub: to the URL append: `?w=1` (`&w=1` if other <params> present)

---
+++ src/IndexBuffer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Utility.hpp"
+
+#include <GL/glew.h>
+
+class IndexBuffer
+{
+	unsigned int m_rendererId;
+	unsigned int m_count;
+
+public:
+	IndexBuffer(const unsigned int *data, unsigned int count) : m_count(count)
+	{
+		ASSERT(sizeof(m_count) == sizeof(GLuint));
+
+		GLCall(glGenBuffers(1, &m_rendererId));
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_rendererId));
+		GLCall(glBufferData(GL_ARRAY_BUFFER, count * sizeof(float), data, GL_STATIC_DRAW));
+	}
+
+	~IndexBuffer() { GLCall(glDeleteBuffers(1, &m_rendererId)); }
+
+	void Bind() const { GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_rendererId)); }
+	void Unbind() const { GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)); }
+
+	unsigned int GetCount() const { return m_count; }
+};
