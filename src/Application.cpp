a7aa09a792e29bbe4f17c797e656b57ae7739f59
Daniil <Daniilsemen@gmail.com>
2022-07-04 09:28:33 +0300

    e13: Abstracting OpenGL into Classes
    Note: Rename `Renderer.hpp`->`Utility.hpp` in the sake of common sense
    
    Advice:
     - use: `git --ignore-all-space <commit>` (or `-w`) to hide changes caused by an additional "{scope}"
     - in case of GitHub: to the URL append: `?w=1` (`&w=1` if other <params> present)

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,3 +1,7 @@
+#include "Utility.hpp"
+#include "VertexBuffer.hpp"
+#include "IndexBuffer.hpp"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -7,75 +11,6 @@
 #include <string>
 #include <filesystem>
 #include <type_traits>
-
-
-#ifdef _WIN32 // debug-breakpoint/platform-specific
-#include <intrin.h>
-#define BREAKPOINT() __debugbreak()
-#elif defined (__unix__)  || (defined (__APPLE__) && defined (__MACH__))
-#include <signal.h>
-#define BREAKPOINT() raise(SIGTRAP)
-#else // see other methods: https://stackoverflow.com/questions/173618/is-there-a-portable-equivalent-to-debugbreak-debugbreak/49079078#49079078
-//#define BREAKPOINT() // uncomment if no solutions found
-#endif // debug-breakpoint/platform-specific
-
-// Trigger debug breakpoint on `condition` fail
-#define ASSERT(condition) if (!(condition)) BREAKPOINT()
-
-// Assert GL function return type value (signed != -1 && unsigned != 0u) // So genious -_- to not use unsigned everywhere // Metaprogramming == my love :)
-#define GLASSERT(glretval) \
-	static_assert(std::is_arithmetic_v<decltype(glretval)>); \
-	if constexpr (std::is_signed_v    <decltype(glretval)>) { ASSERT(glretval != -1); } \
-	else /* if (unsigned arithmetic type)                */ { ASSERT(glretval != 0u); }
-
-#define GLCall(function_call) \
-    while (glGetError() != GL_NO_ERROR) /* pop */;\
-    function_call;\
-    if (glGetError() != GL_NO_ERROR) {\
-		std::cerr << __FILE__ << ':' << __LINE__ << ": " << #function_call << "\n\n";\
-		BREAKPOINT(); } // <-- scope_end}
-
-
-static void GLAPIENTRY GlDebugMessage_cb(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei, const GLchar *message, const void *)
-{
-	char *source_, *type_, *severity_;
-
-	switch (source)
-	{
-	case GL_DEBUG_SOURCE_API:             source_ = "API";             break;
-	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   source_ = "WINDOW_SYSTEM";   break;
-	case GL_DEBUG_SOURCE_SHADER_COMPILER: source_ = "SHADER_COMPILER"; break;
-	case GL_DEBUG_SOURCE_THIRD_PARTY:     source_ = "THIRD_PARTY";     break;
-	case GL_DEBUG_SOURCE_APPLICATION:     source_ = "APPLICATION";     break;
-	case GL_DEBUG_SOURCE_OTHER:           source_ = "OTHER";           break;
-	default:                              source_ = "<SOURCE>";        break;
-	}
-
-	switch (type)
-	{
-	case GL_DEBUG_TYPE_ERROR:               type_ = "ERROR";               break;
-	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: type_ = "DEPRECATED_BEHAVIOR"; break;
-	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  type_ = "UDEFINED_BEHAVIOR";   break;
-	case GL_DEBUG_TYPE_PORTABILITY:         type_ = "PORTABILITY";         break;
-	case GL_DEBUG_TYPE_PERFORMANCE:         type_ = "PERFORMANCE";         break;
-	case GL_DEBUG_TYPE_OTHER:               type_ = "OTHER";               break;
-	case GL_DEBUG_TYPE_MARKER:              type_ = "MARKER";              break;
-	default:                                type_ = "<TYPE>";              break;
-	}
-
-	switch (severity)
-	{
-	case GL_DEBUG_SEVERITY_HIGH:         severity_ = "HIGH";         break;
-	case GL_DEBUG_SEVERITY_MEDIUM:       severity_ = "MEDIUM";       break;
-	case GL_DEBUG_SEVERITY_LOW:          severity_ = "LOW";          break;
-	case GL_DEBUG_SEVERITY_NOTIFICATION: severity_ = "NOTIFICATION"; return; //break;
-	default:                             severity_ = "<SEVERITY>";   break;
-	}
-
-	std::cerr << std::setw(4) << id << ": GL " << severity_ << ' ' << type_ << " (" << source_ << "): " << message << std::endl;
-
-	// Do not `Breakpoint()` here since callstack is missing/wired/untraceble for Nvidia drivers
-}
 
 struct ShaderProgramSource
 {
@@ -197,75 +132,73 @@ int main(void)
 	std::cout << "Info: GPU  vendor : " << glGetString(GL_VENDOR) << std::endl;
 	std::cout << "Info: Renderer    : " << glGetString(GL_RENDERER) << std::endl;
 
-	float positions[] = { // pos[x,y...]
-		-0.5f, -0.5f, // 0
-		 0.5f, -0.5f, // 1
-		 0.5f,  0.5f, // 2
-		-0.5f,  0.5f, // 3
-	};
+	{ // Vertex-/Index-Buffer scope
+		float positions[] = { // pos[x,y...]
+			-0.5f, -0.5f, // 0
+			 0.5f, -0.5f, // 1
+			 0.5f,  0.5f, // 2
+			-0.5f,  0.5f, // 3
+		};
 
-	unsigned int indices[] = { // trig[v1,v2,v3...]
-		0, 1, 2,
-		2, 3, 0
-	};
+		unsigned int indices[] = { // trig[v1,v2,v3...]
+			0, 1, 2,
+			2, 3, 0
+		};
 
-	unsigned int vao;
-	GLCall(glGenVertexArrays(1, &vao));
-	GLCall(glBindVertexArray(vao));
-
-	unsigned int buffer;
-	GLCall(glGenBuffers(1, &buffer));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
-
-	GLCall(glEnableVertexAttribArray(0));
-	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, reinterpret_cast<const void *>(0))); // links `buffer` to `vao`
-
-	unsigned int ibo;
-	GLCall(glGenBuffers(1, &ibo));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
-
-	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-	unsigned int shader = CreateShader(source.vertexSource, source.fragmentSource);
-	GLCall(glUseProgram(shader));
-
-	int location;
-	GLCall(location = glGetUniformLocation(shader, "u_Color"));
-	GLASSERT(location); //ASSERT(location != -1);
-	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
-
-	GLCall(glBindVertexArray(0));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-	GLCall(glUseProgram(0));
-
-	float r = 0.0f;
-	float increment = 0.05f;
-	while (!glfwWindowShouldClose(window))
-	{
-		GLCall(glClear(GL_COLOR_BUFFER_BIT));
-
-		GLCall(glUseProgram(shader));
-		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
-
+		unsigned int vao;
+		GLCall(glGenVertexArrays(1, &vao));
 		GLCall(glBindVertexArray(vao));
-		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 
-		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+		static_assert(sizeof(float) == sizeof(std::remove_extent_t<decltype(positions)>));
+		VertexBuffer vb(positions, 4 * 2 * sizeof(std::remove_extent_t<decltype(positions)>));
 
-		if (r > 1.0f)
-			increment = -0.05f;
-		else if (r < 0.0f)
-			increment = +0.05f;
-		r += increment;
+		GLCall(glEnableVertexAttribArray(0));
+		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, reinterpret_cast<const void *>(0))); // links `buffer` to `vao`
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		IndexBuffer ib(indices, 6);
 
-	} // while (!glfwWindowShouldClose(window))
+		ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+		unsigned int shader = CreateShader(source.vertexSource, source.fragmentSource);
+		GLCall(glUseProgram(shader));
 
-	GLCall(glDeleteProgram(shader));
+		int location;
+		GLCall(location = glGetUniformLocation(shader, "u_Color"));
+		GLASSERT(location); //ASSERT(location != -1);
+		GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+		GLCall(glBindVertexArray(0));
+		vb.Unbind();
+		ib.Unbind();
+		GLCall(glUseProgram(0));
+
+		float r = 0.0f;
+		float increment = 0.05f;
+		while (!glfwWindowShouldClose(window))
+		{
+			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+			GLCall(glUseProgram(shader));
+			GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+			GLCall(glBindVertexArray(vao));
+			ib.Bind();
+
+			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+			if (r > 1.0f)
+				increment = -0.05f;
+			else if (r < 0.0f)
+				increment = +0.05f;
+			r += increment;
+
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+
+		} // while (!glfwWindowShouldClose(window))
+
+		GLCall(glDeleteProgram(shader));
+
+	} // Vertex-/Index-Buffer scope
 
 	glfwTerminate();
 }
