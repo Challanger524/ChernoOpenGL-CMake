7b5235478a60f0cd6c0f6467519258ebfd3cceb9
Daniil <Daniilsemen@gmail.com>
2022-07-10 14:17:46 +0300

    e17: Textures in OpenGL
    Note: about textures slot limitation, there also a term - "Bindless Texture"

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,6 +5,7 @@
 #include "VertexArray.hpp"
 #include "Shader.hpp"
 #include "Renderer.hpp"
+#include "Texture.hpp"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -61,6 +62,10 @@ int main(void)
 		std::cout << "Info: GL: Extension: GL_KHR_debug - OK\n";
 	}
 
+	// capability: blending
+	GLCall(glEnable(GL_BLEND));
+	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 	std::cout << std::endl;
 
 	std::cout << "Info: GLFW version: " << glfwGetVersionString() << std::endl;
@@ -72,10 +77,10 @@ int main(void)
 
 	{ // Vertex-/Index-Buffer scope
 		float positions[] = { // pos[x,y...]
-			-0.5f, -0.5f, // 0
-			 0.5f, -0.5f, // 1
-			 0.5f,  0.5f, // 2
-			-0.5f,  0.5f, // 3
+			-0.5f, -0.5f, 0.0f, 0.0f, // 0
+			 0.5f, -0.5f, 1.0f, 0.0f, // 1
+			 0.5f,  0.5f, 1.0f, 1.0f, // 2
+			-0.5f,  0.5f, 0.0f, 1.0f, // 3
 		};
 
 		unsigned int indices[] = { // trig[v1,v2,v3...]
@@ -84,18 +89,22 @@ int main(void)
 		};
 
 		static_assert(sizeof(float) == sizeof(std::remove_extent_t<decltype(positions)>));
-		VertexBuffer vb(positions, 4 * 2 * sizeof(std::remove_extent_t<decltype(positions)>));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(std::remove_extent_t<decltype(positions)>));
 		VertexArray va;
 		VertexBufferLayout layout;
 
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
+		Texture texture("res/textures/ChernoLogo.png");
+		texture.Bind(0);
+
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniform1i("u_Texture", 0);
 
 		va.Unbind();
 		vb.Unbind();
@@ -104,22 +113,13 @@ int main(void)
 
 		Renderer renderer;
 
-		float r = 0.0f;
-		float increment = 0.05f;
 		while (!glfwWindowShouldClose(window))
 		{
 			renderer.Clear();
 
 			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
 			renderer.Draw(va, ib, shader);
-
-			if (r > 1.0f)
-				increment = -0.05f;
-			else if (r < 0.0f)
-				increment = +0.05f;
-			r += increment;
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
