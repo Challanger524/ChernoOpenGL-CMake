a3d8a42111f4c9d519beb6e5ebe47edb09b797ae
Daniil <Daniilsemen@gmail.com>
2022-07-10 14:17:46 +0300

    e17: Textures in OpenGL
    Note: about textures slot limitation, there also a term - "Bindless Texture"

--- src/Shader.hpp
+++ src/Shader.hpp
@@ -34,6 +34,7 @@ public:
 	void Bind() const { GLCall(glUseProgram(m_RendererId)); }
 	void Unbind() const { GLCall(glUseProgram(0)); }
 
+	void SetUniform1i(const std::string &name, int value) /*                        */ { GLCall(glUniform1i(GetUniformLocation(name), value)); }
 	void SetUniform4f(const std::string &name, float v0, float v1, float v2, float v3) { GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3)); }
 
 private:
