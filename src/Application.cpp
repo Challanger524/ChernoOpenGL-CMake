64f00440cfd5cce9115f7775dc2c2178620664fa
Daniil <Daniilsemen@gmail.com>
2022-07-10 14:17:46 +0300

    e17: Textures in OpenGL
    Note: about textures slot limitation, there also a term - "Bindless Texture"

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,6 +5,7 @@
 #include "VertexArray.hpp"
 #include "Shader.hpp"
 #include "Renderer.hpp"
+#include "Texture.hpp"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -59,6 +60,10 @@ int main()
 		std::cout << "Info: GL: Extension: GL_KHR_debug - OK\n";
 	}
 
+	// capability: blending
+	GLCall(glEnable(GL_BLEND));
+	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 	std::cout << std::endl;
 
 	std::cout << "Info: GLFW version: " << glfwGetVersionString() << std::endl;
@@ -70,10 +75,10 @@ int main()
 
 	{ // Vertex-/Index-Buffer scope
 		float positions[] = { // pos[x,y...]
-			-0.5f, -0.5f, // 0
-			 0.5f, -0.5f, // 1
-			 0.5f,  0.5f, // 2
-			-0.5f,  0.5f, // 3
+			-0.5f, -0.5f, 0.0f, 0.0f, // 0
+			 0.5f, -0.5f, 1.0f, 0.0f, // 1
+			 0.5f,  0.5f, 1.0f, 1.0f, // 2
+			-0.5f,  0.5f, 0.0f, 1.0f, // 3
 		};
 
 		unsigned int indices[] = { // trig[v1,v2,v3...]
@@ -82,18 +87,22 @@ int main()
 		};
 
 		static_assert(sizeof(float) == sizeof(std::remove_extent_t<decltype(positions)>));
-		VertexBuffer vb(positions, 4 * 2 * sizeof(std::remove_extent_t<decltype(positions)>));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(std::remove_extent_t<decltype(positions)>));
 		VertexArray va;
 		VertexBufferLayout layout;
 
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
+		Texture texture("res/textures/ChernoLogo.png");
+		texture.Bind(0);
+
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniform1i("u_Texture", 0);
 
 		va.Unbind();
 		vb.Unbind();
@@ -102,22 +111,13 @@ int main()
 
 		Renderer renderer;
 
-		float r = 0.0f;
-		float increment = 0.05f;
 		while (!glfwWindowShouldClose(window))
 		{
 			renderer.Clear();
 
 			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
 			renderer.Draw(va, ib, shader);
-
-			if (r > 1.0f)
-				increment = -0.05f;
-			else if (r < 0.0f)
-				increment = +0.05f;
-			r += increment;
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
---
+++ src/Texture.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Utility.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+#include <GL/glew.h>
+
+#include <filesystem>
+
+class Texture
+{
+	unsigned int m_rendererId = 0;
+	std::filesystem::path m_filePath;
+	unsigned char *m_localBuffer = nullptr;
+	int m_width = 0, m_height = 0, m_bpp = 0;
+
+public:
+	Texture(const std::filesystem::path &path) : m_filePath(path)
+	{
+		stbi_set_flip_vertically_on_load(true);
+		m_localBuffer = stbi_load(m_filePath.string().c_str(), &m_width, &m_height, &m_bpp, STBI_rgb_alpha);
+
+		GLCall(glGenTextures(1, &m_rendererId));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_rendererId));
+
+		// setup deafult texture settings
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+		GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+
+		GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_localBuffer));
+		GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+
+		if (m_localBuffer)
+			stbi_image_free(m_localBuffer);
+	}
+	~Texture() { GLCall(glDeleteTextures(1, &m_rendererId)); }
+
+	void Bind(unsigned int slot) const
+	{
+		GLCall(glActiveTexture(GL_TEXTURE0 + slot));
+		GLCall(glBindTexture(GL_TEXTURE_2D, m_rendererId));
+		// also there are a term - "Bindless Texture"
+	}
+	void Unbind() const { GLCall(glBindTexture(GL_TEXTURE_2D, 0)); }
+
+	int GetWidth() const { return m_width; }
+	int GetHeight() const { return m_height; }
+};
