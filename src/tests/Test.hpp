4e78f501a71dbba799234613534491d39a511f3f
Daniil <Daniilsemen@gmail.com>
2022-07-20 09:57:07 +0300

    e25: Creating Tests in OpenGL

--- src/tests/Test.hpp
+++ src/tests/Test.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <imgui/imgui.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+
 namespace test
 {
 
@@ -12,6 +19,36 @@ public:
 	virtual void OnUpdate(float deltaTime = 0.0f) {}
 	virtual void OnRender() {}
 	virtual void OnImGuiRender() {}
+};
+
+class TestMenu : public Test
+{
+	using tests_t = std::vector<std::pair<std::string, std::function<Test *()>>>;
+
+	Test *&m_currentTest;
+	tests_t m_tests;
+
+public:
+	TestMenu(Test *&currentTestPtr) : m_currentTest(currentTestPtr) {}
+	virtual ~TestMenu() override {}
+
+	virtual void OnUpdate(float deltaTime = 0.0f) override {}
+	virtual void OnRender() override {}
+	virtual void OnImGuiRender() override
+	{
+		for (auto &test : m_tests)
+		{
+			if (ImGui::Button(test.first.c_str()))
+				m_currentTest = test.second();
+		}
+	}
+
+	template<class T>
+	void RegisterTest(const std::string &name)
+	{
+		std::cout << "Trace: Registering test: " << name << '\n';
+		m_tests.push_back(std::make_pair(name, []() { return new T(); }));
+	}
 };
 
 }
